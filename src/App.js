d8aef8e605c084a212be88b82238e93ecb71c92e
Ranipandit <rani17@navgurukul.org>
2019-02-28 19:53:53 +0530

    scripting done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from './components/header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
+        <Header />
        <h1>JailTube</h1>
       </div>
     );
